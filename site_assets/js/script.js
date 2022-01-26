7d355d8b54aabf0370444eb627ff63e362c31bed
Neeraj Chekutty <64561854+NeerajC05@users.noreply.github.com>
2022-01-26 17:39:16 +0000

    Changed teams

--- site_assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-     document.getElementById("xyz").innerHTML = "BLAHH";
-     $('#navbar ul a').each(function() {
-      if (this.href === path) {
-       $(this).addClass('active');
-      }
-     });
-});
